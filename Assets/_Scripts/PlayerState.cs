999bdaa774970725e719af7917ed43ee76d81a16
emilyperegrine <tomfperegrine@googlemail.com>
2015-11-28 19:00:46 +0000

    Added enums for character state

---
+++ Assets/_Scripts/PlayerState.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerState : MonoBehaviour {
+
+	CharacterState CurrentState;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+enum CharacterState {
+	Grounded,
+	Walking,
+	Sprinting,
+	Jumping,
+	Falling
+}
